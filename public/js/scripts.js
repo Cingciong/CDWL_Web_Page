00d588c36c9eb40926b4ab4632f92e64ef80f9ab
Cingciong <kulakesikula@gmail.com>
2022-08-12 22:56:13 +0200

    last bits of the page, before finishing up

---
+++ public/js/scripts.js
@@ -0,0 +1,95 @@
+
+function button1() {
+    const mediacontainer = document.getElementById('mediacontainer');
+    mediacontainer.innerHTML=`<div class="image-holder">
+    <div class="half-image-holder">
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+    </div>
+    <div class="half-image-holder">
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+        <div class="image-matrix">
+            <img src="/assets/img/page-in-work.png">
+        </div>
+    </div>
+</div>`;
+    console.log('button-1')
+}
+
+function button2() {
+    const mediacontainer = document.getElementById('mediacontainer');
+    mediacontainer.innerHTML=`<div class="image-holder">
+    <div class="half-image-holder">
+        <img src="/assets/img/page-in-work.png">
+    </div>
+    <div class="half-image-holder">
+        <img src="/assets/img/page-in-work.png">
+    </div>
+</div>`;
+    console.log('button-2')
+}
+
+function button3() {
+    const mediacontainer = document.getElementById('mediacontainer');
+    mediacontainer.innerHTML=`<div class="image-holder">
+                    <div class="half-image-holder">
+                        <img src="/assets/img/arty/concept-art-3.png">
+                    </div>
+                    <div class="half-image-holder">
+                        <div class="image-matrix">
+                            <img src="/assets/img/arty/concept-art-2.gif">
+                        </div>
+                         <div class="image-matrix">
+                             <img src="/assets/img/arty/concept-art-1.gif">
+                         </div>
+                         <div class="image-matrix">
+                            <img src="/assets/img/page-in-work.png">
+                        </div>
+                         <div class="image-matrix">
+                            <img src="/assets/img/page-in-work.png">
+                         </div>
+                </div>
+            </div>`
+    console.log('button-3')
+}
+
+function button4() {
+    const mediacontainer = document.getElementById('mediacontainer');
+    mediacontainer.innerHTML=` <div class="image-holder">
+    <div class="half-image-holder">
+                        <div class="image-matrix">
+                            <img src="/assets/img/arty/concept-art-2.gif">
+                        </div>
+                         <div class="image-matrix">
+                             <img src="/assets/img/arty/concept-art-1.gif">
+                         </div>
+                         <div class="image-matrix">
+                            <img src="/assets/img/page-in-work.png">
+                        </div>
+                         <div class="image-matrix">
+                            <img src="/assets/img/page-in-work.png">
+                         </div>
+                </div>
+                <div class="half-image-holder">
+                <img src="/assets/img/arty/concept-art-3.png">
+            </div> </div>`;
+    console.log('button-4')
+}
